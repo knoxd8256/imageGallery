49c07f63f96d95cad8e2c00bf8d5a41726bf9cd9
David Knox <dakdave26@gmail.com>
2020-04-25 16:16:20 -0400

    API complete (for now).

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,36 @@ var MongoClient = new mongodb_1.default.MongoClient('mongodb://localhost:27017',
 MongoClient.connect(function (error) { return error ? console.log(error.message) : console.log("Successfully Connected to the Database!"); });
 var db = MongoClient.db('imageGallery');
 var collection = db.collection('images');
+// Response shorthand function.
 function respond(content, response, code) {
     if (code === void 0) { code = 200; }
     response.status(code);
     response.send(content);
     return;
 }
+// Post type checker.
+function isPost(content, callback) {
+    if (typeof content.title != 'string') {
+        callback(false, 'title must be a string');
+        return;
+    }
+    if (typeof content.imageUrl != 'string') {
+        callback(false, "imageUrl must be a string.");
+        return;
+    }
+    if (typeof content.description != 'string') {
+        callback(false, "description must be a string.");
+        return;
+    }
+    if (typeof content.tags != 'object') {
+        callback(false, "tags must be an array.");
+        return;
+    }
+    callback(true, 'This is a post.');
+    return;
+}
 // Routes.
+// Post getter route.
 app.post('/getPosts', function (req, res) {
     // Filter to tags if there are any.
     var filter = req.body.tags ? { 'tags': { $in: req.body.tags } } : {};
@@ -36,33 +59,76 @@ app.post('/getPosts', function (req, res) {
     });
     return;
 });
+// Post adder route.
 app.post('/addPost', function (req, res) {
     var content = req.body;
-    if (typeof content.title != 'string') {
-        respond("title must be a string.", res, 400);
+    isPost(content, function (result, resText) {
+        if (result === true) {
+            collection.insertOne(content, function (error) {
+                if (error) {
+                    console.log(error.message);
+                    respond("There was an error adding your post.", res, 500);
+                }
+                else {
+                    console.log("Successfully added post!");
+                    respond("Successfully added post!", res);
+                }
+            });
+            return;
+        }
+        respond(resText, res, 400);
+        return;
+    });
+});
+// Post deletion route.
+app.post('/deletePost', function (req, res) {
+    var rawPostId = req.body._id;
+    var postId;
+    if (mongodb_1.default.ObjectId.isValid(rawPostId)) {
+        postId = new mongodb_1.default.ObjectId(rawPostId);
+    }
+    else {
+        respond('Invalid ID.', res, 400);
         return;
     }
-    if (typeof content.imageUrl != 'string') {
-        respond("imageUrl must be a string.", res, 400);
-        return;
-    }
-    if (typeof content.description != 'string') {
-        respond("description must be a string.", res, 400);
-        return;
-    }
-    if (typeof content.tags != 'object') {
-        respond("tags must be an array.", res, 400);
-        return;
-    }
-    collection.insertOne(content, function (error, res) {
-        if (error) {
-            console.log(error.message);
+    collection.findOneAndDelete({ _id: postId }, function (err, result) {
+        if (err) {
+            console.log(err.message);
+            respond("Could not delete post.", res, 500);
+        }
+        if (result.value == null) {
+            respond("No such document found.", res, 400);
         }
         else {
-            console.log("Successfully added post!");
+            respond("Deleted post!", res);
+            return;
         }
     });
-    respond("Successfully added post!", res);
+});
+// Post update route.
+app.post('/updatePost', function (req, res) {
+    var content = req.body;
+    isPost(content, function (result, resText) {
+        if (mongodb_1.default.ObjectId.isValid(req.body._id) && result == true) {
+            content._id = new mongodb_1.default.ObjectId(req.body._id);
+            collection.replaceOne({ "_id": content._id }, content, function (err, result) {
+                if (err) {
+                    console.log(err.message);
+                    respond("There was and error updating the document.", res, 500);
+                }
+                else {
+                    if (result.matchedCount == 0) {
+                        respond("No such document found", res, 400);
+                        return;
+                    }
+                    respond("Document successfully updated!", res);
+                }
+            });
+        }
+        else {
+            respond(result ? "Invalid Id." : resText, res, 400);
+        }
+    });
 });
 // Start API.
 app.listen(9001);
--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,66 @@
 // Imports.
 import express from 'express';
-import mongodb, { MongoError } from 'mongodb';
+import mongodb from 'mongodb';
 
 // Create application.
 const app = express();
 app.use(express.json());
 
 // MongoDB Client.
-const MongoClient = new mongodb.MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
+const MongoClient = new mongodb.MongoClient('mongodb://localhost:27017', { useUnifiedTopology: true });
 MongoClient.connect((error) => error ? console.log(error.message) : console.log("Successfully Connected to the Database!"));
 const db = MongoClient.db('imageGallery');
 const collection = db.collection('images');
 
+// Response shorthand function.
 function respond(content: any, response: express.Response<any>, code: number = 200) {
     response.status(code)
     response.send(content)
     return;
 }
 
+// Post interfaces.
+interface PostCallback {
+    (errorPresent: Boolean, resolutionText: String): void
+}
+
+interface Post {
+    _id?: string | mongodb.ObjectId,
+    title: string,
+    imageUrl: string,
+    description: string,
+    tags: Array<string>
+}
+
+// Post type checker.
+function isPost(content: Post, callback: PostCallback): void {
+    if (typeof content.title != 'string') {
+        callback(false, 'title must be a string')
+        return;
+    }
+    if (typeof content.imageUrl != 'string') {
+        callback(false, "imageUrl must be a string.")
+        return;
+    }
+    if (typeof content.description != 'string') {
+        callback(false, "description must be a string.")
+        return;
+    }
+    if (typeof content.tags != 'object') {
+        callback(false, "tags must be an array.")
+        return;
+    }
+    callback(true, 'This is a post.');
+    return;
+}
+
 
 // Routes.
+
+// Post getter route.
 app.post('/getPosts', (req, res) => {
     // Filter to tags if there are any.
-    const filter = req.body.tags ? {'tags': { $in: req.body.tags }} : {};
+    const filter = req.body.tags ? { 'tags': { $in: req.body.tags } } : {};
     // Return the results or handle any errors which are present.
     collection.find(filter).toArray((error, posts) => {
         if (error) {
@@ -36,33 +74,75 @@ app.post('/getPosts', (req, res) => {
     return;
 });
 
+// Post adder route.
 app.post('/addPost', (req, res) => {
-    const content = req.body;
-    if (typeof content.title != 'string') {
-        respond("title must be a string.", res, 400);
-        return;
-    }
-    if (typeof content.imageUrl != 'string') {
-        respond("imageUrl must be a string.", res, 400);
-        return;
-    }
-    if (typeof content.description != 'string') {
-        respond("description must be a string.", res, 400);
-        return;
-    }
-    if (typeof content.tags != 'object') {
-        respond("tags must be an array.", res, 400);
-        return;
-    }
-    collection.insertOne(content, (error, res) => {
-        if (error) {
-            console.log(error.message);
+    const content = req.body as Post;
+    isPost(content, (result, resText) => {
+        if (result === true) {
+            collection.insertOne(content, (error) => {
+                if (error) {
+                    console.log(error.message);
+                    respond("There was an error adding your post.", res, 500)
+                }
+                else {
+                    console.log("Successfully added post!");
+                    respond("Successfully added post!", res);
+                }
+            });
+            return;
         }
-        else {
-            console.log("Successfully added post!");
+        respond(resText, res, 400);
+        return;
+    });
+});
+
+// Post deletion route.
+app.post('/deletePost', (req, res) => {
+    const rawPostId = req.body._id;
+    let postId: mongodb.ObjectId;
+    if (mongodb.ObjectId.isValid(rawPostId)) {
+        postId = new mongodb.ObjectId(rawPostId);
+    } else {
+        respond('Invalid ID.', res, 400);
+        return;
+    }
+    collection.findOneAndDelete({_id: postId}, (err, result) => {
+        if (err) {
+            console.log(err.message);
+            respond("Could not delete post.", res, 500);
+        }
+        if (result.value == null) {
+            respond("No such document found.", res, 400)
+        } else {
+            respond("Deleted post!", res)
+            return;
         }
     });
-    respond("Successfully added post!", res);
 });
+
+// Post update route.
+app.post('/updatePost', (req, res) => {
+    const content = req.body as Post;
+    isPost(content, (result, resText) => {
+    if (mongodb.ObjectId.isValid(req.body._id) && result == true) {
+        content._id = new mongodb.ObjectId(req.body._id);
+        collection.replaceOne({"_id": content._id}, content, (err, result) => {
+            if (err) {
+                console.log(err.message);
+                respond("There was and error updating the document.", res, 500);
+            } else {
+                if (result.matchedCount == 0) {
+                    respond("No such document found.", res, 400);
+                    return;
+                }
+                respond("Document successfully updated!", res);
+            }
+        })
+    } else {
+        respond(result ? "Invalid Id." : resText, res, 400);
+    }
+});
+});
+
 // Start API.
 app.listen(9001);
